f5e833ac14eabd4e99a0dd21e954c4ecc65b189e
All <allvb@rambler.ru>
2023-03-19 11:32:48 +0300

    Опять исправлено восьмое задание

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,38 +1,53 @@
 const signIn = document.getElementById('signin__form');//элемент форма
 
-signIn.addEventListener('submit', (e) => { //подписка на событие
-    e.preventDefault();
+function request(method, url) { // функция запроса
     const formData = new FormData(signIn); //создание объекта формдата
     const xhr = new XMLHttpRequest(); //создание запроса
-    xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
+    xhr.open(method, url);
     xhr.responseType = 'json';
     xhr.send(formData);
-
     xhr.addEventListener('readystatechange', () => { 
         if (xhr.readyState === xhr.DONE) {
-            if (xhr.response.success) { //при удачном входе
-                let id = !localStorage.id ? xhr.response.user_id : localStorage.id; //присвоение id
-                signIn.classList.toggle('signin'); //скрываем форму
-
-                const reg = document.createElement('div'); //добавляем приветствие
-                reg.classList.add('welcome');
-                reg.classList.add('welcome_active');
-                reg.innerHTML = 
-                    `Добро пожаловать, пользователь # ${id}`
-                document.getElementById('signin').append(reg);
-
-                const btn = document.createElement('button'); //создаем кнопку выхода
-                btn.classList.add('btn-out');
-                btn.textContent = 'Выйти';
-                btn.onclick = () => {
-                    location.reload();
+            if (xhr.response.success) {
+                if (!localStorage.id) {
+                    localStorage.id = xhr.response.user_id;
                 }
-                document.getElementById('signin').append(btn);
-             } else { //если неверный пароль/логин
+                login(); // создаем приветствие 
+            } else {
                 alert('Неверный логин/пароль');
                 signIn.reset();
             }
         }
     });
+}
+
+function create(tag, className, classNameActive) { // функция отрисовки 
+    const elem = document.createElement(tag);
+    elem.classList.add(className);
+    elem.classList.add(classNameActive);
+    document.getElementById('signin').append(elem);
+    return elem;
+}
+
+function login () { // функция приветствия
+        signIn.classList.toggle('signin'); //скрываем форму
+            
+        const reg = create('div', 'welcome', 'welcome_active');
+        reg.innerHTML = `Добро пожаловать, пользователь # ${localStorage.id}`;
+
+        const btn = create('button', 'btn-out');
+        btn.textContent = 'Выйти';
+        btn.onclick = () => {
+            localStorage.removeItem('id');
+            location.reload();
+        }
+}        
+
+signIn.addEventListener('submit', (e) => { //подписка на событие
+    e.preventDefault();
+    request('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
 });
 
+if (localStorage.id) { // Если в localStorage.id что-то есть то создаем приветствие
+    login();
+}
